489783edb8958e9a0384c39feaa1da4cb6ea8d68
prabhathkaluarachchi <fmprabhath@gmail.com>
2025-02-10 11:07:43 +0530

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -36,7 +36,7 @@ function About() {
               <h2>Software Engineer (UG)</h2>
               <p className="fst-italic py-3">
                 I'm a driven undergraduate software engineer at the Open
-                University of Sri Lanka.OUSL
+                University of Sri Lanka.
               </p>
               <div className="row">
                 <div className="col-lg-6">
