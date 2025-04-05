224cae9c72b8586152a4cb7e0e6f4e9da90011e4
prabhathkaluarachchi <fmprabhath@gmail.com>
2025-04-05 19:37:24 +0530

    Addd SEO

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet'; // Import Helmet
-
+import { Helmet } from 'react-helmet';
 
 const App = () => {
   return (
@@ -30,9 +29,11 @@ const App = () => {
                 "url": "https://prabhath.online/",
                 "sameAs": [
                   "https://www.linkedin.com/in/prabhathkaluarachchi/",
-                  "https://github.com/prabhathkaluarachchi"
+                  "https://github.com/prabhathkaluarachchi",
+                  "https://www.facebook.com/prabhathtkaluarachchi/",
+                  "https://www.instagram.com/prabhathtkaluarachchi/"
                 ],
-                "jobTitle": "Software Engineer | Frontend Developer",
+                "jobTitle": "Software Engineer | Frontend Developer"
               }
             `}
           </script>
@@ -55,69 +56,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import Home from './pages/Home';
-// import About from './pages/About';
-// import Resume from './pages/Resume';
-// import Portfolio from './pages/Portfolio';
-// import Contact from './pages/Contact';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="min-h-screen flex flex-col">
-//         <Navbar />
-//         <main className="flex-grow">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/resume" element={<Resume />} />
-//             <Route path="/portfolio" element={<Portfolio />} />
-//             <Route path="/contact" element={<Contact />} />
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
-
