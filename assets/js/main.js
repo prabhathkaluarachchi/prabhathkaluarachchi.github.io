0a24c148388e3e67e4059b467558579ff220a061
prabhathkaluarachchi <fmprabhath@gmail.com>
2025-04-15 07:33:54 +0530

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-// // Disable right-click with custom message
-// document.addEventListener("contextmenu", function (event) {
-//   event.preventDefault();
-//   alert("Right Click was disabled by Administrator Prabhath Kaluarachchi");
-// });
-
 /**
  * Apply .scrolled class to the HTML root as the page is scrolled down
  */
